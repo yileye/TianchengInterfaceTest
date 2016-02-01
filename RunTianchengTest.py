4715a49589244189a6475e81d70b64bd9ee62442
Defias <yechaohui@tuandai.com>
2016-02-01 10:54:50 +0800

    update

--- RunTianchengTest.py
+++ RunTianchengTest.py
@@ -12,26 +12,3 @@ if __name__ == '__main__':
     print 'RunTianchengTest Start...'
     TianchengTest.TianchengTest()
     print 'RunTianchengTest End'
-
-    """
-    from src import MockTest
-    MockTest.testHttpServer2()
-    """
-
-    """
-    from src import MockTest
-    import multiprocessing
-    import os
-    print 'MockTest Start...'
-    multiprocessing.freeze_support()
-    print('Parent process %s.' % os.getpid())
-    p = multiprocessing.Process(target=MockTest.testHttpServer)
-    print('Child process will start.')
-    p.start()
-    print "p.pid:", p.pid
-    print "p.name:", p.name
-    print "p.is_alive:", p.is_alive()
-    p.join()
-    print('Child process end.')
-    print 'MockTest End'
-    """
